6fa69e69a927fda607c3e02e9f6e65b34364da9b
Ivan Moran Vazquez <ivamova@hotmail.com>
2025-10-03 11:03:50 +0200

    Visualización de los usuarios

--- loteria.py
+++ loteria.py
@@ -10,3 +10,11 @@ for codigo in range(1, 16):
         "correo": fake.email(),
         "telefono": fake.phone_number()
     }
+
+print("----------Usuarios----------")
+for codigo, datos in usuarios.items():
+    print(f"Usuario {codigo}:")
+    print(f"    Nombre: {datos['nombre']}")
+    print(f"    Dirección: {datos['direccion']}")
+    print(f"    Correo electrónico: {datos['correo']}")
+    print(f"    Teléfono: {datos['telefono']}")
